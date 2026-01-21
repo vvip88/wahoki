2d7fa819075fbbb0336338ad6d3dcd87b1f40e7a
vvip88 <huangleeboy1996@gmail.com>
2026-01-21 12:42:36 +0700

    Delete wahok88-landing directory

--- wahok88-landing/script.js
+++
@@ -1,7 +0,0 @@
-let jackpot = 683288342;
-const el = document.getElementById("jackpot");
-
-setInterval(() => {
-  jackpot += Math.floor(Math.random() * 700);
-  el.textContent = jackpot.toLocaleString("id-ID");
-}, 1000);
